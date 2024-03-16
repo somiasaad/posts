f6885d9b202622c4fab1b25595b37d9b836315e6
somiasaad <111437823+somiasaad@users.noreply.github.com>
2024-03-16 23:13:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors())
 app.use(cors({ origin: "https://post-q0e5.onrender.com",origin:"http://localhost:3000", credentials: true }));
 
 mongoose.set("strictQuery", true);
